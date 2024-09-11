1d62dab5723b95d2c36d2d714ae77e3488bfc902
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-09-11 14:01:50 +0200

    Chore: Created the first useStated from the Pomodoro.

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
+  const [working, setWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO | SHORT | BREAK");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
