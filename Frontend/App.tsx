a0db31b4a54cf561b95e9c09412367871c420bf3
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-09-11 14:24:32 +0200

    Chore: Modfied file.

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, View } from "react-native";
+import Header from "./src/pages/Header"; // Asegúrate de que "Header" tenga las mayúsculas correctas
 
 export default function App() {
   const [working, setWorking] = useState(false);
@@ -9,8 +10,8 @@ export default function App() {
   return (
     <SafeAreaView>
       <View>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <Header />
+        <StatusBar />
       </View>
     </SafeAreaView>
   );
