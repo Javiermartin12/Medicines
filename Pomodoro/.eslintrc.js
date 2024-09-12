5d57b330190119c3ddfb5cde47b99f508528bd69
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-09-12 21:21:12 +0200

    Chore: Added slint modification.

--- Pomodoro/.eslintrc.js
+++ Pomodoro/.eslintrc.js
@@ -1,25 +1,20 @@
 // .eslintrc.js
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-native/all",
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "plugin:react-native/all"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
-      tsx: true,
     },
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react-native", "@typescript-eslint"],
+  plugins: ["react-native"],
   rules: {
     "no-undef": "off",
     "react/react-in-jsx-scope": "off",
