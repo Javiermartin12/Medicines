e4c1fa4e62a1364a71da6f8cd6eb0790c2253dac
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-09-12 12:35:45 +0200

    Chore: Created different options to manage Pomodoro and rest time.

--- Pomodoro/src/components/Header.jsx
+++ Pomodoro/src/components/Header.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-function Header() {
-  return <View>nothing</View>;
+const options = ["Pomodoro", "Short Break", "Long Break"];
+function Header({ time }) {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {options.map((items, index) => (
+        <TouchableOpacity key={index}>
+          <Text>{items}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
 }
 
 export default Header;
+
+const styles = StyleSheet.create({
+  items: {
+    width: "33%",
+  },
+});
