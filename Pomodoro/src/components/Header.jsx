21041bb02552bcbeb297e07da6f113c8548a9f08
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-09-12 13:19:17 +0200

    Chore: Created function handlePress.

--- Pomodoro/src/components/Header.jsx
+++ Pomodoro/src/components/Header.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const options = ["Pomodoro", "Short Break", "Long Break"];
-function Header({ time }) {
+function Header({ setTime, currentTime, setCurrentTime }) {
+  function handlePress(index) {
+    const newTime = index === 0 ? 25 : index === 1 ? 5 : 10;
+    setCurrentTime(index);
+    setTime(newTime * 60);
+  }
   return (
     <View style={{ flexDirection: "row" }}>
       {options.map((items, index) => (
-        <TouchableOpacity key={index}>
+        <TouchableOpacity
+          key={index}
+          style={styles.items}
+          onPress={() => handlePress(index)}
+        >
           <Text>{items}</Text>
         </TouchableOpacity>
       ))}
@@ -19,5 +28,8 @@ export default Header;
 const styles = StyleSheet.create({
   items: {
     width: "33%",
+    borderWidth: 2,
+    borderColor: "black",
+    padding: 3,
   },
 });
