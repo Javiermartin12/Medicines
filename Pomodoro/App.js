2642ca3ade914e3737080b1f1117badfbd3c1fe9
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-09-12 12:35:00 +0200

    Chore: Added safeArea to manage text inside the box.

--- Pomodoro/App.js
+++ Pomodoro/App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Header from "./src/components/Header";
 
 export default function App() {
   const [isWorking, setIsWorking] = useState(false);
   const [time, setTime] = useState(25 * 60);
   const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "LONG");
   return (
-    <View style={styles.container}>
-      <Text>Pomodoro</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text>Pomodoro</Text>
+        <StatusBar style="auto" />
+        <Header time={time} />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -19,6 +23,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
