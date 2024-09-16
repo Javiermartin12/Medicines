105bdafef2e8dd19e44b6ccd70b00d097ef13d0b
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-09-16 11:53:34 +0200

    Chore: Created different BG dinamic.

--- Pomodoro/App.js
+++ Pomodoro/App.js
@@ -3,16 +3,20 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import Header from "./src/components/Header";
 
+const colors = ["#F7DC6F", "A2D9CE", "D7BDE2"];
+
 const App = () => {
   const [isWorking, setIsWorking] = useState(false);
   const [time, setTime] = useState(25 * 60);
   const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "LONG");
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors[currentTime] }]}
+    >
+      <StatusBar style="auto" />
       <View style={styles.container}>
         <Text>Pomodoro</Text>
         <Text>{time}</Text>
-        <StatusBar style="auto" />
         <Header
           currentTime={currentTime}
           setCurrentTime={setCurrentTime}
@@ -27,7 +31,6 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
   },
 });
